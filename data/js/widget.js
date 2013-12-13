b584b9c224a5e46f4384b7424a1d041ec22e2d19
Emily Su <emilyrsu@mit.edu>
2013-12-12 22:50:54 -0500

    Remove debug logging.

--- data/js/widget.js
+++ data/js/widget.js
@@ -1,7 +1,5 @@
-console.log('test');
 self.port.on('showIndicator', function() {
   document.getElementById('indicator').style.display = 'inline';
-  console.log(document.getElementById('indicator').style.display);
 });
 self.port.on('hideIndicator', function() {
   document.getElementById('indicator').style.display = 'none';
